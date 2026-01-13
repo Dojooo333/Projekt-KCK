56748820a61ac890b9b54981e1c67b82e1b1eec7
Dojooo333 <dominik.justyna@yahoo.com>
2026-01-13 01:38:06 +0100

    + Poprawiona strona główna z przedmiotami
    Dodanie przedmiotów u prowadzących

--- index.js
+++ index.js
@@ -54,28 +54,43 @@ app.get('/', async (req, res) => {
 
     try{
         
-        const [userData] = (await pool.query(
+        const [userData] = await pool.query(
             "select * from users where login = ?;",
             login
-        ));
-
-        const [userClassesData] = await pool.query(
-            `select c.id, c.className, c.description, cr.title, cr.firstName, cr.lastName
-            from class_members
-            left join users u on class_members.userID = u.id
-            left join classes c on class_members.classID = c.id
-            left join users cr on c.creatorID = cr.id where u.id = ?;`,
-            [userData[0].id]
         );
 
-        //console.log(userClassesData);
+        if(userData[0].isLecturer == 0){
 
-        console.log(userClassesData);
+            // Zwykły uczeń
+            const [userClassesData] = await pool.query(
+                `select c.id, c.className, c.description, cr.title, cr.firstName, cr.lastName
+                from class_members
+                left join users u on class_members.userID = u.id
+                left join classes c on class_members.classID = c.id
+                left join users cr on c.creatorID = cr.id where u.id = ?;`,
+                [userData[0].id]
+            );
 
-        res.render('index.ejs',{
-            userInfo: userData[0],
-            userClasses: userClassesData
-        });
+            res.render('index.ejs',{
+                userInfo: userData[0],
+                userClasses: userClassesData
+            });
+            return;
+
+        }else{
+
+            // Prowadzący
+            const [userClassesData] = await pool.query(
+                `select id, className, description from classes where creatorID = ?;`,
+                [userData[0].id]
+            );
+
+            res.render('index.ejs',{
+                userInfo: userData[0],
+                userClasses: userClassesData
+            });
+            return;
+        }
 
     }catch(err){
         return res.status(500).send('Error fatching data.');
