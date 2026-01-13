b2d7128ec3411d98e437bf8772d56c635187ffb7
Dojooo333 <dominik.justyna@yahoo.com>
2026-01-13 01:05:20 +0100

    + Strona główna z przedmiotami

--- index.js
+++ index.js
@@ -25,47 +25,79 @@ app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'/backend'));
 
 // MySQL
-const mysql = require('mysql2')
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'kck',
-  password: 'l4ce+=f$I$Rc',
-  database: 'kck'
+const mysql = require('mysql2/promise')
+
+const pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'kck',
+    password: 'l4ce+=f$I$Rc',
+    database: 'kck'
 });
 
-connection.connect((err) => {
-    if(err){
+pool.getConnection()
+    .then(conn => {
+        console.log('Connected to MySQL database.');
+        conn.release();
+    }).catch(err => {
         console.error('Database connection failed:\n' + err);
         return;
-    }
-    console.log('Connected to MySQL database.');
-});
-
+    });
 
 //RESTful
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     if(!(req.session && req.session.username)){
         res.redirect('/login');
-    }else{
-        res.send(`Zalogowano jako: ${req.session.username}`);
+        return;
     }
+
+    const login = req.session.username;
+
+    try{
+        
+        const [userData] = (await pool.query(
+            "select * from users where login = ?;",
+            login
+        ));
+
+        const [userClassesData] = await pool.query(
+            `select c.id, c.className, c.description, cr.title, cr.firstName, cr.lastName
+            from class_members
+            left join users u on class_members.userID = u.id
+            left join classes c on class_members.classID = c.id
+            left join users cr on c.creatorID = cr.id where u.id = ?;`,
+            [userData[0].id]
+        );
+
+        //console.log(userClassesData);
+
+        console.log(userClassesData);
+
+        res.render('index.ejs',{
+            userInfo: userData[0],
+            userClasses: userClassesData
+        });
+
+    }catch(err){
+        return res.status(500).send('Error fatching data.');
+    }
+    
 });
 
-app.get('/login', (req, res) => {
+app.get('/login', async (req, res) => {
 
     if(req.session && req.session.username){
         res.redirect('/');
-    }else{
-        const query1 = 'select * from users;';
-        connection.query(query1, (err, rows) => {
-            if(err){
-                return res.status(500).send('Error fatching data.');
-            }
-
-            //res.send(rows);
-            res.render('login.ejs',{rows});
-        });
+        return;
     }
+    try{
+        const query1 = 'select * from users;';
+        const [users] = await pool.query(query1);
+
+        res.render('login.ejs',{usersList: users});
+    }catch(err){
+        return res.status(500).send('Error fatching data.');
+    }
+
 });
 
 app.post('/login', (req, res) => {
